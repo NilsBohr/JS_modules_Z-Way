df5fb69d4d1ff48e5d71e6d27ff100818b49315a
dakota96 <dakota4126@gmail.com>
2021-03-28 17:50:51 +0300

    small fix

--- ConditionerManager/index.js
+++ ConditionerManager/index.js
@@ -35,10 +35,10 @@ ConditionerManager.prototype.init = function (config) {
 			if (command != 'update') {
 				if (command === 'on') {
 					self.vDev.set('metrics:level', command);
-					//zway.devices[self.config.nodeId].instances[self.config.instanceId].commandClasses[self.config.commandClassId].Set(5);
+					zway.devices[self.config.nodeId].instances[self.config.instanceId].commandClasses[self.config.commandClassId].Set(5);
 				} else if (command === 'off') {
 					self.vDev.set('metrics:level', command);
-					//zway.devices[self.config.nodeId].instances[self.config.instanceId].commandClasses[self.config.commandClassId].Set(0);
+					zway.devices[self.config.nodeId].instances[self.config.instanceId].commandClasses[self.config.commandClassId].Set(0);
 				}
 			}
 		},
