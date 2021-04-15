abd43010b26da11f9c44611d64c3fa5e3d0d77b1
dakota96 <dakota4126@gmail.com>
2021-04-15 18:22:40 +0300

    beta-version

--- HomePresence/index.js
+++ HomePresence/index.js
@@ -1,12 +1,3 @@
-/*** Network Presence Z-Way HA module ****************************************
-Version: 0.5
-------------------------------------------------------------------------------
-Author: Kjetil Volden
-Description:
-    This module is used for pinging one or more network devices and determine
-    their presence.
-******************************************************************************/
-
 // ----------------------------------------------------------------------------
 // --- Class definition, inheritance and setup
 // ----------------------------------------------------------------------------
@@ -15,14 +6,6 @@ function HomePresence(id, controller){
     // Call superconstructor first (AutomationModule)
     HomePresence.super_.call(this, id, controller);
     this.icon_path  = '/ZAutomation/api/v1/load/modulemedia/HomePresence/';
-    this.addrs = [];
-    this.motionSensors = [];
-    this.default_advanced_config = {interval: 1,
-                                    netcat_port: 80,
-                                    motion_timeout: 1,
-                                    debug_logging: false};
-    this.crontask_name = undefined;
-    this.langfile = this.controller.loadModuleLang('HomePresence');
 }
 
 inherits(HomePresence, AutomationModule);
@@ -35,19 +18,25 @@ _module = HomePresence;
 // ----------------------------------------------------------------------------
 
 HomePresence.prototype.init = function (config){
-    config.advanced_config = _.extend({}, this.default_advanced_config, config.advanced_config);
-
     HomePresence.super_.prototype.init.call(this, config);
-
     var self = this;
-    this.debug_log = _.bind(this.debug_log, self);
+
+    if (!this.config.interval) {
+        this.config.interval = 1;
+    }
+    if (!this.config.netcat_port) {
+        this.config.netcat_port = 80;
+    }
+    if (!this.config.motion_timeout) {
+        this.config.motion_timeout = 1;
+    }
 
     var dev_id = 'HomePresence_' + self.id;
     var dev_title = 'Home presence status';
     this.vDevPresence = this.controller.devices.create({
         deviceId: dev_id,
         defaults: {
-        deviceType: 'sensorBinary',
+        deviceType: 'switchBinary',
             metrics: {
                 level: 'off',
                 icon: self.icon_path + 'icon.png',
@@ -55,41 +44,53 @@ HomePresence.prototype.init = function (config){
             }
         },
         overlay: {},
-        handler: function (){},
+        handler: function (command){
+            if (command != 'update') {
+				this.set("metrics:level", command);
+				saveObject(this.id + "_level" , command);
+			}
+        },
         moduleId: self.id
     });
 
     this.poll_addr = function () {
         try {
             self.isAddrAlive = false;
-            self.addrs.forEach(function (addr, index) {
-                var p = self.config.advanced_config.netcat_port;
+            self.config.network_addresses.forEach(function (item) {
+                var addr = item.addr;
+
+                var p = self.config.netcat_port;
                 var response = system('netcat -v -z -w ' + '10' + ' ' + addr + ' ' + p + ' 2>&1');
 
-                self.isAddrAlive = self.isAddrAlive || (response[1].indexOf('No route to host') == -1 && (response[1].indexOf('Operation now in progress') == -1));
-                (response[1].indexOf('No route to host') == -1 && (response[1].indexOf('Operation now in progress') == -1)) ? self.debug_log(addr + ' is present. Command output: ' + response[1].slice(0,-1)) : self.debug_log(addr + ' is absent. Command output: ' + response[1].slice(0,-1));
-                    
-                if (self.isAddrAlive) {
-                    if (this.motionTimeout) {
-                        clearTimeout(this.motionTimeout);
-                    }
-
-                    if (self.vDevPresence.get('metrics:level') === 'off') {
-                        self.vDevPresence.set('metrics:level', 'on');
-                    }
-                }
-                    
-                if (!self.isAddrAlive && !self.isMotionPresent && (index === (self.addrs.length - 1))) {
-                    if (self.vDevPresence.get('metrics:level') === 'on') {
-                        self.vDevPresence.set('metrics:level', 'off');
-                    }
-                    self.debug_log('Presence is not detected!');
-                }
-                
-                if (self.isAddrAlive && (index === (self.addrs.length - 1))) {
-                    self.debug_log('Presence detected!');
+                if (response[1].indexOf('No route to host') == -1 && (response[1].indexOf('Operation now in progress') == -1)) {
+                    self.isAddrAlive = true;
+                    self.debug_log(addr + ' is present');
+                } else {
+                    self.debug_log(addr + ' is absent');
                 }
             });
+
+            if (self.isAddrAlive) {
+                if (this.motionTimeout) {
+                    clearTimeout(this.motionTimeout);
+                }
+
+                if (self.vDevPresence.get('metrics:level') === 'off') {
+                    self.vDevPresence.set('metrics:level', 'on');
+                    self.debug_log('Presence switch changed state to ON');
+                } else {
+                    self.debug_log('Presence switch is already ON');
+                }
+            }
+                
+            if (!self.isAddrAlive && !self.isMotionPresent) {
+                if (self.vDevPresence.get('metrics:level') === 'on') {
+                    self.vDevPresence.set('metrics:level', 'off');
+                    self.debug_log('Presence switch changed state to OFF');
+                } else {
+                    self.debug_log('Presence switch is already OFF');
+                }
+            }
         }
         catch(err){
             var msg = 'Home Presence: No permissions to run netcat. Add command "netcat" (without quotes) to .syscommands file.';
@@ -99,22 +100,18 @@ HomePresence.prototype.init = function (config){
                 'module',
                 'HomePresence'
             );
-            self.debug_log('Polling failed, check syscommands');
+            self.debug_log('Polling failed, check .syscommands file');
             return;
         }
     };
 
-    this.config.network_addresses.forEach(function(item) {
-        self.addrs.push(item.addr);
-    });
-
-    var crontime = {minute: [0, 59, config.advanced_config.interval],
+    var crontime = {minute: [0, 59, this.config.interval],
         hour: null,
         weekDay: null,
         day: null,
         month: null};
 
-    this.crontask_name = 'HomePresence_' + self.id + '.poll';
+    this.crontask_name = 'HomePresence_' + this.id + '.poll';
     this.controller.emit('cron.addTask', this.crontask_name, crontime);
     this.controller.on(this.crontask_name, this.poll_addr);
 
@@ -125,40 +122,47 @@ HomePresence.prototype.init = function (config){
     }
 
     this.checkMotion = function () {
-        self.isMotionPresent = false;
-        self.motionSensors.forEach(function(item, index) {
-            var motionSensor = self.controller.devices.get(item);
-            self.isMotionPresent = self.isMotionPresent || motionSensor.get('metrics:level') === 'on' ? true : false;
-            motionSensor.get('metrics:level') === 'on'  ? self.debug_log(motionSensor.deviceId + ' is present') : self.debug_log(motionSensor.deviceId + ' is absent');
-            if (self.isMotionPresent) {
-                if (this.motionTimeout) {
-                    clearTimeout(this.motionTimeout);
-                }
+        self.isMotionPresent = true;
 
-                if (self.vDevPresence.get('metrics:level') === 'off') {
-                    self.vDevPresence.set('metrics:level', 'on');
-                }
-            }
-                
-            if (!self.isMotionPresent && !self.isAddrAlive && (index === (self.addrs.length - 1))) {
-                if (!self.motionTimeout) {
-                    self.motionTimeout = setTimeout(function () {
-                        if (self.vDevPresence.get('metrics:level') === 'on') {
-                            self.vDevPresence.set('metrics:level', 'off');
-                        }
-                    }, config.advanced_config.motion_timeout * 60 * 60 * 1000);
-                }
-                self.debug_log('Motion is absent!');
-            } 
-            
-            if (self.isMotionPresent && (index === (self.addrs.length - 1))) {
-                self.debug_log('Motion is present!');
+        self.config.motion_sensors.forEach(function(dev) {
+            var motionSensor = self.controller.devices.get(dev.motion_sensor);
+
+            if (motionSensor.get('metrics:level') === 'on') {
+                self.isMotionPresent = true;
+                self.debug_log(motionSensor.deviceId + ' is ON');
+            } else {
+                self.debug_log(motionSensor.deviceId + ' is OFF');
             }
         });
+
+        if (self.isMotionPresent) {
+            if (this.motionTimeout) {
+                clearTimeout(this.motionTimeout);
+            }
+
+            if (self.vDevPresence.get('metrics:level') === 'off') {
+                self.vDevPresence.set('metrics:level', 'on');
+                self.debug_log('Presence switch changed state to ON');
+            } else {
+                self.debug_log('Presence switch is already ON');
+            }
+        }
+            
+        if (!self.isMotionPresent && !self.isAddrAlive) {
+            if (!self.motionTimeout) {
+                self.motionTimeout = setTimeout(function () {
+                    if (self.vDevPresence.get('metrics:level') === 'on') {
+                        self.vDevPresence.set('metrics:level', 'off');
+                        self.debug_log('Presence switch changed state to OFF');
+                    } else {
+                        self.debug_log('Presence switch is already OFF');
+                    }
+                }, self.config.motion_timeout * 60 * 60 * 1000);
+            }
+        } 
     }
 
     this.config.motion_sensors.forEach(function(dev) {
-        self.motionSensors.push(dev.motion_sensor);
         self.controller.devices.on(dev.motion_sensor, 'change:metrics:level', self.checkMotion);
     });
 };
@@ -174,12 +178,10 @@ HomePresence.prototype.stop = function (){
     var self = this;
 
     this.controller.devices.remove(this.vDevPresence);
-    this.addrs = [];
-    this.motionSensors = [];
     this.controller.emit('cron.removeTask', this.crontask_name);
     this.controller.off(this.crontask_name, this.poll_addr);
 
-    if (this.timeoutTimer) {
+    if (this.motionTimeout) {
         clearTimeout(this.motionTimeout);
     }
 
@@ -193,7 +195,7 @@ HomePresence.prototype.stop = function (){
 // ----------------------------------------------------------------------------
 
 HomePresence.prototype.debug_log = function (msg){
-    if(this.config.advanced_config.debug_logging){
+    if(this.config.debug_logging){
         console.log('[HomePresence] ' + msg);
     }
 };
